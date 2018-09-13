cd966954b1bf2bffddf377eec43b4047f2b216d0
nelsons <nelson.simao@me.com.br>
2018-09-13 18:06:48 -0300

    Initial Project Add

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
+// --------------------------- main() method ---------------------------
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgPetClinicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
 }
