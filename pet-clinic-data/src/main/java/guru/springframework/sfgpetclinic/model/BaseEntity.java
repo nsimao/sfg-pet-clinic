8e34991b86a866752152f4917104a4ae9d43bbe0
nelsons <nelson.simao@me.com.br>
2018-09-26 18:30:33 -0300

    Base Entity as JPA

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,15 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author Nelson Simão
  * @since 1.0
  */
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 // ------------------------------ FIELDS ------------------------------
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 // --------------------- GETTER / SETTER METHODS ---------------------
